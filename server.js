e3265e9dd27c8a03173cc84922b3ebcc7a498db8
Matthew Landen <mlanden1221@gmail.com>
2021-07-01 16:29:26 -0700

    updated server.js settings

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = require("./app");
 const { PORT } = require("./config");
 
 app.listen(PORT, () => {
-  console.log("server started on http://localhost:3001");
+  console.log(`server started on http://localhost:${PORT}`);
 });
