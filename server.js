75ede6eeb3776d777639ae39eee894216cd095ab
Matthew Landen <mlanden1221@gmail.com>
2021-07-01 16:25:42 -0700

    updated port settings in server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 "use strict";
 
 const app = require("./app");
+const { PORT } = require("./config");
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
   console.log("server started on http://localhost:3001");
 });
