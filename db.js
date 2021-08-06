c7d535711045bbc22dd88e82aaa2bbcaf2e8aa14
Matthew Landen <mlanden1221@gmail.com>
2021-08-05 21:49:16 -0700

    updated database path for production

--- db.js
+++ db.js
@@ -1,22 +1,29 @@
 "use strict";
 /** Database setup for MVE. */
 const { Client } = require("pg");
-const { getDatabaseUri } = require("./config");
+const { DATABASE_URL } = require("./config");
 
 let db;
 
-if (process.env.NODE_ENV === "production") {
-  db = new Client({
-    connectionString: getDatabaseUri(),
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
-} else {
-  db = new Client({
-    connectionString: getDatabaseUri(),
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   db = new Client({
+//     connectionString: getDatabaseUri(),
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+//   });
+// } else {
+//   db = new Client({
+//     connectionString: getDatabaseUri(),
+//   });
+// }
+
+db = new Client({
+  connectionString: DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 db.connect();
 
